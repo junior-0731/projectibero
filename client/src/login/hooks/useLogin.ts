6786930878c0b21b29a627b7d9c5e460c8127729
Junior Herrera <herrerajunior715@gmail.com>
2026-02-01 20:31:51 -0500

    fix(auth): corrección de la lógica de inicio de sesión

--- client/src/login/hooks/useLogin.ts
+++ client/src/login/hooks/useLogin.ts
@@ -1,17 +1,26 @@
+import { toast } from 'react-hot-toast';
 // Creacion de hook que se va encargar de usar el servicio
-
 import { loginMethod } from "../services/auth-service"
-
+import { useNavigate } from "react-router";
+import { useAppContext } from '../../context/useAppContext';
 // Tener metodo que se encargara de la autenticacion
 export const useLogin = () => {
+    const navigate = useNavigate()
+    // Estraer el login
+    const {login} = useAppContext()
     const loginUser = async (form: { username: string; password: string }) => {
         try {
             const { data } = await loginMethod(form.username, form.password);
-            console.log('loginuser', data);
+            // se pasa al estado general, para alamcenar los valores del estado
+            login(data.empleado, data.access_token);
+            localStorage.setItem("token", JSON.stringify(data.access_token));
+            localStorage.setItem("empleado", JSON.stringify(data.empleado));
+            toast.success("Bienvenido");
+            navigate("/")
             return data;
         } catch (error) {
             if (error instanceof Error) {
-                alert(error.message);
+                toast.error(error.message);
             }
             return null;
         }
--- client/src/login/services/auth-service.ts
+++ client/src/login/services/auth-service.ts
@@ -1,8 +1,13 @@
-// Usar metodo apiClient
 import { apiClient } from "../../config/axios-config";
 
-// Crear metodo de login
-
-export const loginMethod = async(userName: string, password:string)=>{
-    return await apiClient.post('/auth/login', {userName,password})
+export interface IRegisterBody{
+    name: string
+    userName: string
+    password: string
 }
+
+export const loginMethod = async (userName: string, password: string) => 
+    await apiClient.post("/auth/login", { userName, password });
+
+export const registerMethod = async (body: IRegisterBody) =>
+    await apiClient.post("/auth/register", body);

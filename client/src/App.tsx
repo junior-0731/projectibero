2e120a194ef00eca40b1ad610df753231bbb20ef
Junior Herrera <herrerajunior715@gmail.com>
2026-02-01 20:31:37 -0500

    refactor(cliente): ajustes en estilos base y estructura de la aplicación

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,23 @@
 import { Routes, Route } from 'react-router'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import { Toaster } from 'react-hot-toast'
+import ProtectedRoute from './components/ProtectedRoute'
 function App() {
 
   return (
    <>
-   <Routes>
-    <Route path='/' element={<HomePage />} />
-    <Route path='/login' element={<LoginPage />} />
-   </Routes>
+    <Toaster position="top-center" />
+    <Routes>
+      <Route path='/'  element={
+         <ProtectedRoute>
+            <HomePage />
+         </ProtectedRoute>
+        } />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage/>}/>
+    </Routes>
    </>
   )
 }
